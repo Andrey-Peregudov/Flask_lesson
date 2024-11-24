6b7d4c31ecd81c4161838973163893909a1d8989
Andrey-Peregudov <anmrya12@gmail.com>
2024-11-24 21:33:54 +0300

    add qpp funtion +10

--- app/main.py
+++ app/main.py
@@ -1,32 +1,111 @@
-import os
+from flask import Flask, render_template, flash, redirect, url_for, request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-from flask import Flask, render_template, flash, redirect, url_for
-
-from forms import LoginForm
+from database import Config
+from forms import LoginForm, TovarForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jjjjjjj'
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
+from models import User, Tovar
+
+with app.app_context():
+    db.create_all()
+    have_user = User.query.first()
+    print(have_user)
+    if not have_user:
+        from seed import seeds
+
+        seeds()
+
+#Главная страница
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # form=GoTovarForm()
+    tovar = Tovar.query.all()
+    return render_template('index.html', tovars=tovar)
 
-
+#Функция регистрации нового пользователя в БД
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form3 = LoginForm()
     if form3.validate_on_submit():
-        name=form3.username.data
-        print(form3.pasword.data)
-        print(form3.remember_me.data)
-
-        flash('YES,' + name + ' Flash flask work!!')
-        flash('YES, Flash flask work22!!')
-        flash('YES, Flash flask work3333!!')
+        name = form3.username.data
+        data = User(name=form3.username.data, password=form3.pasword.data, is_active=False)
+        db.session.add(data)
+        db.session.commit()
+        flash('ПОЛЬЗОВАТЕЛЬ ' + name + ' ЗАРЕГИСТРИРОВАН')
         return redirect(url_for('index'))
     return render_template('login.html', form2=form3)
 
+#Функция добавления товара в БД
+@app.route('/tovar_add', methods=['GET', 'POST'])
+def tovar_add():
+    form = TovarForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        price = form.price.data
+        ostatok = form.ostatok.data
+        data = Tovar(name=name, price=int(price), ostatok=int(ostatok))
+        db.session.add(data)
+        db.session.commit()
+        flash('Товар добавлен')
+        return redirect(url_for('index'))
+    return render_template('tovar_add.html', form=form)
+
+#Функция удаления товара череж URL
+@app.route('/tovar_del/<tovar_id>', methods=['GET', 'POST'])
+def del_tovar(tovar_id: int):
+    data = Tovar.query.get(tovar_id)
+    # data = Tovar.query.select(Tovar.id == tovar_id).one()
+    # data = Tovar.query.where(Tovar.id == tovar_id).one()
+    db.session.delete(data)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+#Функция покупки товаров
+@app.route('/tovar_kupit', methods=['GET', 'POST'])
+def tovar_kupit():
+    id = request.args.get('id')
+    print(id)
+    data = Tovar.query.get(id)
+    data.ostatok = data.ostatok - 1
+    db.session.commit()
+    print(data)
+    return redirect(url_for('index'))
+
+
+@app.route('/add_ten_tovar', methods=['GET', 'POST'])
+def add_ten_tovar():
+    id = request.args.get('id')
+    data = Tovar.query.get(id)
+    data.ostatok = data.ostatok + 10
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+#Страница товара
+@app.route('/tovar_page', methods=['GET', 'POST'])
+def tovar_page():
+    id = request.args.get('id')
+    print(id)
+    data = Tovar.query.get(id)
+    print(data)
+    return render_template('tovar_page.html', data=data)
+
+@app.route('/tovar_new_name/<tovar_id>/<new_name>', methods=['GET', 'POST'])
+def name_tovar(tovar_id: int, new_name: str):
+    data = Tovar.query.get(tovar_id)
+    data.name = new_name
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
+    # with app.app_context():
+    #     db.create_all()
     app.run(port=5001, debug=True)
